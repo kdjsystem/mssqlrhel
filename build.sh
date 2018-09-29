6eefddf13c1f968ed9aad5df1a74d88f0bb517d0
kdjsystem <42904285+kdjsystem@users.noreply.github.com>
2018-09-29 12:16:25 +0900

    commit

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sleep 60
 oc new-project mssql 
 oadm policy add-scc-to-user anyuid -z default
 oc new-app https://github.com/kdjsystem/mssqlrhel 
-oc env "dc/mssqlrhel" -e ACCEPT_EULA=y -e SA_PASSWORD=RedHat123!
+oc env "dc/mssqlrhel" -e ACCEPT_EULA=y -e SA_PASSWORD='zpdlelwpdl12!@'
 oc volume "dc/mssqlrhel" --add --mount-path=/var/opt/mssql --type=persistentVolumeClaim --claim-name=mssqlrhel --claim-mode="ReadWriteOnce" --claim-size=1G
 sleep 20
 oc logs -f bc/mssqlrhel
